574ee4f6c77a5d5ea6817f2824eea31d2710d2c8
nilesh426 <nileshasawant426@gmail.com>
2026-06-17 01:22:58 +0530

    change

--- Problem_solving/day3.py
+++ Problem_solving/day3.py
@@ -1,4 +1,12 @@
-bill=[3,10,2,9]
-print((sum(bill)-bill[1])%2)
+# bill=[3,10,2,9]
+# print((sum(bill)-bill[1])/2)
 
 
+unsorted=['1','200','150','3']
+for i in range(len(unsorted)):
+    for j in range(len(unsorted)):
+        if unsorted[i] > unsorted[j]:
+            unsorted[i], unsorted[j] = unsorted[j], unsorted[i]
+
+print(unsorted)           
+        
